edad74005a4d108ee951e1275be04ee1393708cd
Romane Green <Romane71193@gmail.com>
2018-03-29 02:22:39 -0400

    eleminated markings

--- public/hard.js
+++ public/hard.js
@@ -22,7 +22,7 @@ function showCoords(event) {
   var x = event.clientX;
   var y = event.clientY;
   var coords = "X coords: " + x + ", Y coords: " + y;
-  document.getElementById("info").innerHTML = coords;
+//   document.getElementById("info").innerHTML = coords;
   if (x > 714 && x <= 730) {
     alert("Congrats!You found Wally!");
     window.history.back();
--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ function showCoords(event) {
   var x = event.clientX;
   var y = event.clientY;
   var coords = "X coords: " + x + ", Y coords: " + y;
-  document.getElementById("info").innerHTML = coords;
+//   document.getElementById("info").innerHTML = coords;
   if (x > 856 && x <= 945) {
     alert("Congrats!You found Wally!");
     window.history.back();
