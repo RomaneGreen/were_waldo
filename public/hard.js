559475dc190753a8797d0aef36e54c77de3a07ff
Romane Green <Romane71193@gmail.com>
2018-04-04 13:13:49 -0400

    linted

--- public/hard.js
+++ public/hard.js
@@ -1,34 +1,36 @@
 
 function timer() {
-  let sec = 5;
-  const timer = setInterval(() => {
-    document.getElementById("timerdisplay").innerHTML =
+    let sec = 5;
+    const timers = setInterval(() => {
+        document.getElementById("timerdisplay").innerHTML =
       `Time left:00:${sec}`;
-    sec--;
-    if (sec < 1) {
-      clearInterval(timer);
-      alert("sorry times up!");
-      window.history.back();
-    }
-    if (sec < 10) {
-      document.getElementById("timerdisplay").innerHTML =
+        sec--;
+        if (sec < 1) {
+            clearInterval(timers);
+            alert("sorry times up!");
+            window.history.back();
+        }
+        if (sec < 10) {
+            document.getElementById("timerdisplay").innerHTML =
         `Time left:00:0${sec}`;
-    }
-  }, 1000);
+        }
+    },1000);
 }
 
 window.onload = timer();
 
-//get cordinates
+// Get coordinates
 function showCoords(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  const coords = `X coords: ${x}, Y coords: ${y}`;
-//   document.getElementById("info").innerHTML = coords; user shouldnt see coordinates
-  if (x > 714 && x <= 730) {
-    alert("Congrats!You found Wally!");
-    window.history.back();
-  }
+    const x = event.clientX,
+        y = event.clientY,
+        //  Coords = `X coords: ${x}, Y coords: ${y}`;
+    // Document.getElementById("info").innerHTML = coords;  user shouldnt see coordinates 
+
+    // eslint-disable-next-line Parsing error: Unexpected keyword 'if'
+    if (x > 714 && x <= 730) { 
+        alert("Congrats!You found Wally!");
+        window.history.back();
+    }
 }
 
 showCoords();
--- public/medium.js
+++ public/medium.js
@@ -1,37 +1,35 @@
-
-
-
-//sets the timer
+// Sets the timer
 function timer() {
-  let sec = 15;
-  const timer = setInterval(() => {
-    document.getElementById("timerdisplay").innerHTML =
+    let sec = 15;
+    const timers = setInterval(() => {
+        document.getElementById("timerdisplay").innerHTML =
       `Time left:00:${sec}`;
-    sec--;
-    if (sec < 1) {
-      clearInterval(timer);
-      alert("sorry times up!");
-      window.history.back();
-    }
-    if (sec < 10) {
-      document.getElementById("timerdisplay").innerHTML =
+        sec--;
+        if (sec < 1) {
+            clearInterval(timers);
+            alert("sorry times up!");
+            window.history.back();
+        }
+        if (sec < 10) {
+            document.getElementById("timerdisplay").innerHTML =
         `Time left:00:0${sec}`;
-    }
-  }, 1000);
+        }
+    },1000);
 }
 
 window.onload = timer();
 
-//gets coordinates
+// Gets coordinates
 function showCoords(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  const coords = `X coords: ${x}, Y coords: ${y}`;
-//   document.getElementById("info").innerHTML = coords; //user shouldnt see coordinates
-  if (x >= 551 && x <= 570) {
-    alert("Congrats!You found Wally!");
-    window.history.back();
-  }
+    const x = event.clientX,
+        y = event.clientY,
+        // coords = `X coords: ${x}, Y coords: ${y}`;
+    //   Document.getElementById("info").innerHTML = coords; //user shouldnt see coordinates
+    
+            if (x >= 551 && x <= 570) {  
+        alert("Congrats!You found Wally!");
+        window.history.back();
+    }
 }
 
 showCoords();
